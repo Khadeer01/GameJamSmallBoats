45dec824e2c5baa78d9f5fb71a424ded8cdec331
yooooan <cc7831q@gre.ac.uk>
2025-11-07 07:18:58 +0000

    Started to implement a score system
    Only finished the game manager, need to implement the ui itself and scripts alongside with it later

---
+++ Drugsmug/Assets/ScoringSystem/GameManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private int currentScore = 0;
+
+    public int CurrentScore => currentScore;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddScore(int amount)
+    {
+        currentScore += amount;
+        Debug.Log($"Score added: {amount}, Total Score: {currentScore}");
+
+        // implement this when we have a score ui
+        // ScoreUI scoreUI = FindFirstObjectByType<ScoreUI>();
+        // if (scoreUI != null) {
+            // scoreUI.UpdateScoreText(currentScore);
+    }
+
+    // public void ResetScore()
+    // {
+        // currentScore = 0;
+        // ScoreUI scoreUI = FindFirstObjectByType<ScoreUI>();
+        // if (scoreUI != null)
+        // {
+            // scoreUI.UpdateScoreText(currentScore);
+        // }
+    // }
+}
