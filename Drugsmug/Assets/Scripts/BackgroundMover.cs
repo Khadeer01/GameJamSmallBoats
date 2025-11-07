07413748155ce2df35d40051189bccc214651158
Kreedomz <vt4560w@gre.ac.uk>
2025-11-07 17:11:24 +0000

    Made a fully working and transitioning background moving for the entire duration of the game

--- Drugsmug/Assets/Scripts/BackgroundMover.cs
+++ Drugsmug/Assets/Scripts/BackgroundMover.cs
@@ -1,57 +1,93 @@
+using NUnit.Framework;
 using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BackgroundMover : MonoBehaviour
 {
     [SerializeField] float backgroundMoveSpeed = 1.0f;
 
-    [SerializeField] SpriteRenderer background01;
-    [SerializeField] SpriteRenderer background02;
-
-    SpriteRenderer currentMainMovingBackground;
+    [SerializeField] SpriteRenderer[] backgrounds;
+    Vector2[] backgroundDefaultLocations;
 
     Vector2 startPosition = Vector2.zero;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentMainMovingBackground = background01;
-        startPosition = currentMainMovingBackground.transform.position;
+        StoreBackgroundDefaultLocations();
+    }
+
+    void StoreBackgroundDefaultLocations()
+    {
+        if (backgrounds == null || backgrounds.Length == 0) { return; }
+        backgroundDefaultLocations = new Vector2[backgrounds.Length];
+
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            backgroundDefaultLocations[i] = backgrounds[i].transform.position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Vector2 movePosition = new Vector2(0.0f, )
-        currentMainMovingBackground.transform.position += Vector3.down * backgroundMoveSpeed * Time.deltaTime;
-        FollowOtherBackground();
-    }
+        CheckEachBackgroundIsOutsideCamera();
+	}
 
-    void FollowOtherBackground()
+    void CheckEachBackgroundIsOutsideCamera()
     {
-        if (currentMainMovingBackground == background01)
+        if (backgrounds.Length > 0 && Camera.main != null)
         {
-            Vector2 topLeft = new Vector3(
-                background02.bounds.min.x, // left
-                background02.bounds.max.y // top
-            );
+            for (int i = 0; i < backgrounds.Length; i++) 
+            {
+                SpriteRenderer background = backgrounds[i];
 
+				background.transform.position += Vector3.down * backgroundMoveSpeed * Time.deltaTime;
 
-            Vector2 topLeftScreenVector = new Vector2(0.0f, Screen.height);
-            float outOfScreenMag = (topLeftScreenVector - topLeft).magnitude ;
-            print("Screen distance: " + outOfScreenMag);
-            //Camera camera = Camera.main;
-            //if (camera != null)
+                Vector3 topPositionOfBackground = background.transform.position + new Vector3(0.0f, background.bounds.extents.y, 0.0f);
 
-            //{
-            //    if (camera.WorldToScreenPoint()
-            //}        
-        }
-        else if (currentMainMovingBackground != background02)
-        {
+				Vector3 backgroundViewportPos = Camera.main.WorldToViewportPoint(topPositionOfBackground);
+				bool isOffScreen = backgroundViewportPos.x < 0 || backgroundViewportPos.x > 1 || backgroundViewportPos.y < 0;
+                if (isOffScreen)
+                {
+                    SpriteRenderer nextBackground = GetNextAvailableBackground(background);
+                    if (nextBackground != null)
+                    {
+						Vector3 topPositionOfNextBackground = nextBackground.transform.position + new Vector3(0.0f, nextBackground.bounds.extents.y*2, 0.0f);
 
+						background.transform.position = topPositionOfNextBackground;
+					}
+				}
+			}
         }
     }
 
-    
-}
+    SpriteRenderer GetNextAvailableBackground(SpriteRenderer oldBackground)
+    {
+        if (backgrounds.Length == 0) return null;
+
+        int nextIndex = 0;
+
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            SpriteRenderer currentBackground = backgrounds[i];
+
+            if (oldBackground == currentBackground)
+            {
+                nextIndex = i;
+                if (i >= backgrounds.Length - 1)
+                {
+					nextIndex = 0;
+                }
+                else
+                {
+                    nextIndex = nextIndex + 1;
+                }
+            }
+
+        }
+
+        return backgrounds[nextIndex];
+    }
+  }
